017acb0fc380becf7babe43040ac79e5054a14bc
Anton <103072519+onen-touw@users.noreply.github.com>
2024-02-04 18:43:31 +0300

    Update2 main.py

--- lab3/main.py
+++ lab3/main.py
@@ -5,10 +5,10 @@ class Book(object):
         self.__name = name
         self.__author = author
 
-    def get_name(self):
+    def get_name(self) -> str:
         return self.__name
 
-    def get_author(self):
+    def get_author(self) -> str:
         return self.__author
 
     def __str__(self):
@@ -19,29 +19,51 @@ class Book(object):
 
 
 class PaperBook(Book):
+    """ Класс бумажная книга (Наследник Book). """
     def __init__(self, name: str, author: str, pages: int):
-        if pages < 0:
-            raise ValueError("Колличество страниц не может быть < 0")
-        elif not isinstance(pages, int):
-            raise TypeError
         super().__init__(name, author)
         self.pages = pages
 
     def __repr__(self):
-        return f"{self.__class__.__name__}(name={self.get_name()!r}, author={self.get_author()!r}, pages={self.pages!r})"
+        return f"{self.__class__.__name__}(name={self.get_name()!r}, author={self.get_author()!r}, " \
+               f"pages={self._pages!r})"
+
+    @property
+    def pages(self) -> int:
+        return self._pages
+
+    @pages.setter
+    def pages(self, pages: int) -> None:
+        # print("pages::setter")
+        if pages < 0:
+            raise ValueError("Колличество страниц не может быть < 0")
+        elif not isinstance(pages, int):
+            raise TypeError
+        self._pages = pages
 
 
 class AudioBook(Book):
+    """ Класс аудио книга (Наследник Book). """
     def __init__(self, name: str, author: str, duration: float):
-        if duration < 0:
-            raise ValueError("Длительность не может быть < 0")
-        elif not isinstance(duration, float):
-            raise TypeError
         super().__init__(name, author)
         self.duration = duration
 
     def __repr__(self):
-        return f"{self.__class__.__name__}(name={self.get_name()!r}, author={self.get_author()!r}, duration={self.duration!r})"
+        return f"{self.__class__.__name__}(name={self.get_name()!r}, author={self.get_author()!r}," \
+               f" duration={self._duration!r})"
+
+    @property
+    def duration(self):
+        return self._duration
+
+    @duration.setter
+    def duration(self, duration: float) -> None:
+        # print("duration::setter")
+        if duration < 0:
+            raise ValueError("Длительность не может быть < 0")
+        elif not isinstance(duration, float):
+            raise TypeError
+        self._duration = duration
 
 
 #                                   TEST START
@@ -59,17 +81,17 @@ BOOKS_DATABASE = [
 ]
 
 if __name__ == '__main__':
-    pBook = PaperBook("name", "auth", 10)
+    pBook = PaperBook("pName", "pAuth", 10)
     print(pBook)
-    aBook = AudioBook("name", "auth", 10.)
+    print(pBook.pages, "\n")
+
+    aBook = AudioBook("aName", "aAuth", 10.)
     print(aBook)
+    print(aBook.duration, "\n")
 
     list_books = [
         PaperBook(name=book_dict["name"], author=book_dict["author"], pages=book_dict["pages"]) for book_dict in
         BOOKS_DATABASE
     ]
     print(list_books)
-
-    aBook = AudioBook("aName", "aAuthor", 10.31)
-    print(aBook)
 #                                     TEST END
